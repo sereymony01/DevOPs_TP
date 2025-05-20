5df5dfe7f51fb8a907532ccd8bd556e1bafe4489
sereymony01 <sereymony0112@gmail.com>
2025-05-20 21:47:22 +0700

    TP1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,5 @@
 package com.example;
-
+import javax.swing.*;
 /**
  * Hello world!
  *
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame frame = new JFrame("Desktop App");
+        JLabel label = new JLabel("Hello from Maven Java GUI!", SwingConstants.CENTER);
+        frame.add(label);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
